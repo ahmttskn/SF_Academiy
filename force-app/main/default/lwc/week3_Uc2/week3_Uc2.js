db72607f18f4b853c34616da5cb24ba45641b311
Atskn <atskn@Atskns-MacBook-Air.local>
2023-12-19 19:04:16 -0800

    new update

--- force-app/main/default/lwc/week3_Uc2/week3_Uc2.js
+++ force-app/main/default/lwc/week3_Uc2/week3_Uc2.js
@@ -8,6 +8,8 @@ export default class Week3_Uc2 extends LightningElement {
     selectedAccId; 
     error;
     total;
+    tOpp;
+    noOpp;
 
     @wire(getAllAccounts)
     accountHandler({data, error}) {
@@ -30,7 +32,24 @@ export default class Week3_Uc2 extends LightningElement {
         this.selectedAccId = event.target.value;
         calculateOppAmount({accId: this.selectedAccId})
             .then(result => {
-                this.total = result;
+                //this.total = result;
+                const num = 0;
+                if (result > 0) {
+                    this.total = result;
+                    this.tOpp = undefined;
+                    this.noOpp = undefined;
+                }
+                if (result === num) {
+                    console.log(result);
+                    this.tOpp = ' ';
+                    this.noOpp = undefined;
+                    this.total = undefined;
+                }
+                if (result < 0) {
+                    this.noOpp = result;
+                    this.total = undefined;
+                    this.tOpp = undefined;  
+                }  
             })
             .catch(error => {
                 this.error = error;
